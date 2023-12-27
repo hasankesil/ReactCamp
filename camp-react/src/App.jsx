2587cf80bb3f6356ec67524f3df876eed5712fbb
hasankesil <hasankesil@hotmail.com>
2023-12-27 13:02:42 +0300

    Fundamentals Items created

---
+++ camp-react/src/App.jsx
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { Container } from 'semantic-ui-react'
+import Dashboard from './layouts/Dashboard';
+import Navi from './layouts/Navi';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navi></Navi>
+      <Container className='main'>
+        <Dashboard></Dashboard>
+      </Container>
+
+
+    </div>
+  );
+}
+
+export default App;
