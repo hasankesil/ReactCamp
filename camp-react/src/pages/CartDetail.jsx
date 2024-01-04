ef2d346ae054c4f424008f318cfd204615f6167d
hasankesil <hasankesil@hotmail.com>
2024-01-04 14:10:37 +0300

    CartDetail added and edited

--- camp-react/src/pages/CartDetail.jsx
+++ camp-react/src/pages/CartDetail.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { Card, Image, Button } from 'semantic-ui-react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, removeFromCart } from '../store/actions/cartActions';
 
 export default function CartDetail() {
+    const dispatch = useDispatch();
+    const { cartItems } = useSelector(state => state.cart);
+
+    const handleAddToCart = (todo) => {
+        dispatch(addToCart(todo));
+    }
+
+    const handleRemoveFromCart = (todo) => {
+        dispatch(removeFromCart(todo));
+    }
+
     return (
         <div>
-            Sepet detayı
+            {cartItems.map((cartItem) => (
+                <Card fluid >
+                    <Image
+                        floated='right'
+                        size='mini'
+                        src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
+                    />
+                    <Card.Content>
+                        <Card.Header>{`Name: ${cartItem.todo.title}`}</Card.Header>
+                        <Card.Meta style={{ fontSize: '1.5em', fontWeight: 'bold' }}>{`Quantity: ${cartItem.quantity}`}</Card.Meta>
+
+                        <Card.Description>
+                            {`Description: ${cartItem.todo.description}`}
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <div className='ui two buttons'>
+                            <Button basic color='green' onClick={() => handleAddToCart(cartItem.todo)}>
+                                Arttır
+                            </Button>
+                            <Button basic color='blue' onClick={() => handleRemoveFromCart(cartItem.todo)}>
+                                Azalt
+                            </Button>
+                        </div>
+                    </Card.Content>
+                </Card>
+            ))}
         </div>
-    )
+    );
 }
+
+
+
