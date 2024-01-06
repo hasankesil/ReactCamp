54c386313da52d2fc0425ea9fdd54c7ba9271f7a
hasankesil <hasankesil@hotmail.com>
2024-01-06 22:41:05 +0300

    CartSummery modified

--- camp-react/src/layouts/CartSummery.jsx
+++ camp-react/src/layouts/CartSummery.jsx
@@ -1,30 +1,64 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
-import { Dropdown, DropdownDivider, DropdownItem, Label } from 'semantic-ui-react'
+// CartSummery.jsx
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { Dropdown, DropdownDivider, DropdownItem, Label } from 'semantic-ui-react';
+import { useDispatch } from 'react-redux';
+import { addToCart, removeFromCart } from '../store/actions/cartActions';
+import { toast } from 'react-toastify';
+
+
 
 export default function CartSummery() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
-  const { cartItems } = useSelector(state => state.cart)
+  const handleAddToCart = (todo) => {
+    dispatch(addToCart(todo));
+    toast.success(`${todo.userId} sepete eklendi`, {
+      autoClose: 1200,
+    });
+  };
+
+  const handleRemoveFromCart = (todo) => {
+    dispatch(removeFromCart(todo));
+    toast.success(`${todo.userId} sepetten çıkarıldı`, {
+      autoClose: 1200,
+    });
+  };
+
   return (
     <div>
       <Dropdown item text='Sepetiniz'>
         <Dropdown.Menu>
-          {
-            cartItems.map((cartItem) => (
+          {cartItems.map((cartItem) => (
+            <Dropdown.Item key={cartItem.todo.userId}>
+              {cartItem.todo.userId} numaralı kullanıcı {'(ürün)'}: {'  '}
+              <Label>{cartItem.quantity}</Label>
 
-              <Dropdown.Item>   {cartItem.todo.userId} numaralı kullanıcı {'(ürün)'}: {'  '}
-                <Label>
-                  {cartItem.quantity}
+
+              <span style={{ display: 'flex', alignItems: 'center', marginLeft: '8px' }}>
+                <Label as='a' onClick={() => handleAddToCart(cartItem.todo)} color='green' tag>
+                  Arttır
                 </Label>
+                <Label as='a' onClick={() => handleRemoveFromCart(cartItem.todo)} color='red' tag style={{ transform: 'rotate(180deg)', display: 'inline-block' }}>
+                  <div style={{ transform: 'rotate(180deg)', display: 'inline-block' }}>
+                    Azalt
+                  </div>
+                </Label>
+              </span>
 
-              </Dropdown.Item>
-            ))
-          }
+
+
+            </Dropdown.Item>
+          ))}
           <DropdownDivider />
-          <DropdownItem as={NavLink} to="/cart" >Sepete Git</DropdownItem>
+          <DropdownItem as={NavLink} to='/cart'>
+            Sepete Git
+          </DropdownItem>
         </Dropdown.Menu>
       </Dropdown>
-    </div >
-  )
+    </div>
+  );
 }
